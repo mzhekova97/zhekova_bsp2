a14ffbcaca7805386b8b2faf57cd6bd9e8b3bd5b
mzhekova97 <mzhekova97@gmail.com>
2019-05-28 20:11:12 +0200

    changed on 28/05/2019

--- stack/traffic_node/scripts/door_listen.py
+++ stack/traffic_node/scripts/door_listen.py
@@ -7,9 +7,6 @@ import closeDoor
 import RPi.GPIO as GPIO
 
 def callback(msg):
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	GPIO.setup(21, GPIO.OUT)
 	pwm = GPIO.PWM(21, 50)
 	rospy.loginfo(rospy.get_caller_id() +" "+ str(msg.data))
 	if(msg.data==0):
@@ -19,12 +16,15 @@ def callback(msg):
 		
 def listener():
 
-    rospy.init_node('door_listen', anonymous=True)
+	rospy.init_node('door_listen', anonymous=True)
 
-    rospy.Subscriber('keypress_door', Int8, callback)
-	
+	rospy.Subscriber('keypress_door', Int8, callback)
+
+ 	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+	GPIO.setup(21, GPIO.OUT)
     # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+	rospy.spin()
 
 if __name__ == '__main__':
 	try:
--- stack/traffic_node/scripts/led_decision.py
+++ stack/traffic_node/scripts/led_decision.py
@@ -6,29 +6,29 @@ from std_msgs.msg import Float32
 from std_msgs.msg import String
 import RPi.GPIO as GPIO
 
+ledRed = 17
+ledYellow = 18
+ledGreen = 22
+
 def callback(msg):
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-
-	ledRed = 17
-	ledYellow = 18
-	ledGreen = 22
-
-	GPIO.setup(ledRed, GPIO.OUT) 
-	GPIO.setup(ledYellow, GPIO.OUT) 
-	GPIO.setup(ledGreen, GPIO.OUT)
 	pub = rospy.Publisher('color_of_led', String, queue_size=10)
 	rospy.loginfo(rospy.get_caller_id() + " I heard: "+ str(msg.data))
 	resp=led.traffic(msg.data,ledRed,ledYellow,ledGreen) 
 	pub.publish(resp)
 
-	
+
 def listener_talker():
-    rospy.init_node('led_decision', anonymous=True)
-    rospy.Subscriber('distance_from_sensor', Float32, callback)
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
 	
+	GPIO.setup(ledRed, GPIO.OUT) 
+	GPIO.setup(ledYellow, GPIO.OUT) 
+	GPIO.setup(ledGreen, GPIO.OUT)
+	
+	rospy.init_node('led_decision', anonymous=True)
+	rospy.Subscriber('distance_from_sensor', Float32, callback)
     # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+	rospy.spin()
 
 if __name__ == '__main__':
 	try:
